131c7c17f595bc787a1c05faa09ebddfc3843504
IMenezes <107199192+IMenezes-GH@users.noreply.github.com>
2023-11-07 13:23:51 -0300

    Translates jsdoc and Error message to portuguese

--- middleware/VerifyRole.js
+++ middleware/VerifyRole.js
@@ -1,3 +1,8 @@
+/**
+ * Compara se o role presente na JWT está na lista de roles definida pela função. Se sim, permite o uso da rota, caso não retorna 401 (unauthorized) se o JWT não for encontrado, ou 403 (forbidden) se o usuário não tiver permissão para usar o recurso.
+ * @param  {...string} roles Lista de roles permitidos para dada rota
+ * @returns void
+ */
 const verifyRole = (...roles) => {
     return (req, res, next) => {
         console.log(req.userData)
--- util/parseUtil.js
+++ util/parseUtil.js
@@ -1,15 +1,18 @@
 
 /**
- * Parses 'true' or 'false' strings to Boolean
- * @param {string} str A string with expected 'true' or 'false' value
+ * Converte 'true' ou 'false' strings para Boolean
+ * @param {string} str String com o valor esperado 'true' ou 'false'
  * @returns {boolean}
  */
 export const stringToBool = (str) => {
-    if (!str || str === undefined) throw new Error('Cannot convert to Bool: A value is required.');
+    if (!str || str === undefined) throw new Error('Não foi possível converter para boolean, valor indefinido.');
 
     const allowedStrings = ['true', 'false'];
     if (allowedStrings.includes(str.toLowerCase())){
         if (str === 'true') return true;
         if (str === 'false') return false;
+    }
+    else {
+        throw new Error('Valor esperado: "true" ou "false". Valor recebido: ', str);
     }
 }
